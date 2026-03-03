e7a1144c4dd86aa739f1b156247f73433a1f1dad
Valentin Bosch <val.bosch2001@gmail.com>
2026-03-03 11:09:20 +0100

    up

--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
-#!/bin/ksh 
+#!/bin/ksh -1
 #$ -q gpu
 #$ -j y
 #$ -o result.out
-#$ -N clamv2
+#$ -N mil_train
 cd $WORKDIR
 cd /beegfs/data/work/c-2iia/vb710264/mil_lab
 source /beegfs/data/work/c-2iia/vb710264/mil_lab/milab_venv/bin/activate
-module load python
-export PYTHONPATH=/work/c-2iia/vb710264/mil_lab/milab_venv/lib/python3.9/site-packages:$PYTHONPATH
+#module load python
+#export PYTHONPATH=/work/c-2iia/vb710264/mil_lab/milab_venv/lib/python3.9/site-packages:$PYTHONPATH
 export MPLCONFIGDIR=/work/c-2iia/vb710264/.cache/matplotlib
 
 python /beegfs/data/work/c-2iia/vb710264/mil_lab/MIL-Lab/src/train.py
